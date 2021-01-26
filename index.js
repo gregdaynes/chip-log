443d46bffe064e792c9e2ea6e15860667476c9b2
Gregory Daynes <gregdaynes@gmail.com>
2021-01-25 18:54:53 -0800

    feat: Base logger with context hooks
    Provide main logging module that determins the caller of the log and builds out a structured log. if an async hook namespace is created, and their is a REQUEST_ID property stored, it will append that to the attributes of the log message.

---
+++ index.js
@@ -0,0 +1,40 @@
+const pino = require('pino')
+const cls = require('cls-hooked')
+const memoize = require('lodash.memoize')
+const moduleIntrospector = require('./module-introspector')
+
+const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
+const APP_NAMESPACE = process.env.APP_NAMESPACE || 'APP_NAMESPACE'
+const APP_BASE = process.env.NODE_PATH || process.cwd()
+const LOG_VOID = process.env.LOG_VOID
+
+module.exports = {
+  _logFn,
+  _namespace,
+  trace: (msg, mrg) => (_logFn.bind(null, 'trace')(msg, mrg)),
+  info: (msg, mrg) => (_logFn.bind(null, 'info')(msg, mrg)),
+  warn: (msg, mrg) => (_logFn.bind(null, 'warn')(msg, mrg)),
+  error: (msg, mrg) => (_logFn.bind(null, 'error')(msg, mrg)),
+  fatal: (msg, mrg) => (_logFn.bind(null, 'fatal')(msg, mrg)),
+  debug: (msg, mrg) => (_logFn.bind(null, 'debug')(msg, mrg))
+}
+
+const Pino = pino({
+  level: LOG_LEVEL || 'info',
+  redact: ['req.body.password']
+})
+
+function _logFn (logLevel, message, mergeObject = null, logger = Pino, introspector = moduleIntrospector.initializer, basePath = APP_BASE) {
+  if (LOG_VOID) return
+
+  const [fn, module, lineNum] = introspector(basePath, 5)()
+  const namespace = _namespace()
+  const requestId = (namespace) ? namespace.get('REQUEST_ID') : null
+
+  return logger.child({ module, function: fn, lineNum, requestId })[logLevel](mergeObject, message)
+}
+
+const memoizedNamespace = memoize(cls.getNamespace)
+function _namespace (namespace = APP_NAMESPACE) {
+  return memoizedNamespace(namespace)
+}
---
+++ index.test.js
@@ -0,0 +1,61 @@
+const { _logFn, _namespace } = require('./index')
+const cls = require('cls-hooked')
+
+const namespace = 'APP_NAMESPACE'
+
+beforeAll(() => {
+  cls.createNamespace(namespace)
+})
+
+describe('_logFn', () => {
+  it('calls the parent logger through child', (done) => {
+    const appNamespace = cls.getNamespace(namespace)
+    appNamespace.run(() => runTestWithContext())
+
+    function runTestWithContext () {
+      const requestId = 'abc123'
+      appNamespace.set('REQUEST_ID', requestId)
+
+      const logLevel = 'info'
+      const logger = {
+        child: jest.fn(() => ({
+          [logLevel]: jest.fn()
+        }))
+      }
+
+      const introspector = () => jest.fn(() => {
+        return [
+          'testFn',
+          'this.test.file',
+          666
+        ]
+      })
+
+      _logFn(logLevel, 'under test', {}, logger, introspector, 'basePath')
+
+      expect(logger.child).toHaveBeenCalledWith({
+        function: 'testFn',
+        lineNum: 666,
+        module: 'this.test.file',
+        requestId
+      })
+
+      done()
+    }
+  })
+})
+
+describe('_namespace', () => {
+  it('fetches the namespace from async hook', () => {
+    const actual = _namespace(namespace).name
+
+    expect(actual).toBe(namespace)
+  })
+
+  it('uses memoization on multiple calls', () => {
+    const actual = _namespace(namespace)
+    const actualTwo = _namespace(namespace)
+
+    expect(actual).toBe(actualTwo)
+  })
+})
